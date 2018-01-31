e0b5173aa32d11a0976ce7077f8b32b3ab40d733
yuanyuan <doule.customer@gmail.com>
2018-01-31 10:18:51 +0800

    add to git

---
+++ app_config.php
@@ -0,0 +1,11 @@
+<?php
+$client_id='5a6aa0844b6d3c4aa2ec2814';
+$client_secret= '0a7d82ca3d48401fa09abf0d069aa41b';
+$OAUTH = 'https://dev.worktile.com/api/oauth2/authorize';
+$GET_ACCESS_TOKEN = 'https://dev.worktile.com/api/oauth2/token';
+$INBOX = 'https://dev.worktile.com/api/mission/tasks/inbox';
+$TASKS = 'https://dev.worktile.com/api/mission/tasks?pid=';
+
+//https://aihelp.worktile.com/tasks/projects/5a6ed3ffe44f516d525c29aa
+$PID = '5a6ed3ffe44f516d525c29aa';
+?>
